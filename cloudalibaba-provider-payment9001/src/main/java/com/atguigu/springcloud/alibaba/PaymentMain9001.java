39fa6c556633ba96da68d8ff2106b605c23564ce
renwujie <renwujie@utopin.cn>
2021-05-13 17:39:11 +0800

    Nacos本地启动报错原因，增加。

--- cloudalibaba-provider-payment9001/src/main/java/com/atguigu/springcloud/alibaba/PaymentMain9001.java
+++ cloudalibaba-provider-payment9001/src/main/java/com/atguigu/springcloud/alibaba/PaymentMain9001.java
@@ -11,6 +11,11 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  *      pom添加依赖，配置appication.yml中的而参数，主启动类添加 @EnableDiscoveryClient
  *      只需要下载nacos，解压启动$NACOS_HOME/bin/startup.cmd/sh 即可
  *      localhost:8848/nacos -》 serviceManagement -》 service list 即可查看到服务已注册
+ *
+ *      nacos下载后本地直接启动会报错：WebServerException: Unable to start embedded Tomcat
+ *      因为nacos默认以集群模式启动。mode="cluster"
+ *      需要以命令：startup.cmd/sh -m standalone 启动
+ *      或者修改start.cmd/sh脚本的mode="cluster"的值为standalone，在执行start.cmd/sh启动
  */
 @EnableDiscoveryClient
 @SpringBootApplication
